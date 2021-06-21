1c9efd556e4a1062b2a69bd8a1861746fbe1da80
Lastor-Chen <lastoraya@gmail.com>
2021-06-21 17:47:33 +0800

    [加入] typescript 與 CDN Bootstrap v5

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-md-3">左</div>
+            <div className="col-md-9">右</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
